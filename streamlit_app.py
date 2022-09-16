a5abaf7dc65b06defe1afcd660f01d9ed0364df9
Peyton Critchfield <113618939+peyton-critchfield@users.noreply.github.com>
2022-09-16 10:46:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,11 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # normalizes semi-structured JSON data into a flat table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays normalized data
 streamlit.dataframe(fruityvice_normalized)
+
